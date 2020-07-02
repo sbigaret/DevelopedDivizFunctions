d9cf9363264faf61ab11a455ad3f6fb91e0f1f92
Sébastien Bigaret <sebastien.bigaret@telecom-bretagne.eu>
2020-07-02 10:45:09 +0200

    Remove the parameter 'active'
    There is no need for a parameter 'active', my bad, in the guidelines I
    gave there was a misleading point leading to the introduction of such
    a parameter --but this is not something needed nor expected in the
    context of teh Decision Deck Web-Services.

--- OWA/src/inputsHandler.R
+++ OWA/src/inputsHandler.R
@@ -36,7 +36,6 @@ checkAndExtractInputs <- function(xmcdaData, programExecutionResult) {
     weights <- NULL
     alternativesIDs <- NULL
     criteriaIDs <- NULL
-    activeParameters <- FALSE
     orness <- NULL
     weights <- NULL
 
@@ -99,25 +98,19 @@ checkAndExtractInputs <- function(xmcdaData, programExecutionResult) {
     hasWeights <- (xmcdaData$criteriaValuesList$size() >= 1)
     parameters <- getProgramParametersList(xmcdaData)
     hasParameters <- (length(parameters) == 1)
-    
+
     if(hasParameters){
       parameters <- parameters[[1]]
       #control if the document structure is correct
       aux <- names(parameters)
-      if ((length(aux) != 2) || !("active" %in% aux) || !("orness" %in% aux))
+      if ((length(aux) != 1) || !("orness" %in% aux))
         stop("Error: Estructural error in program parameters file ")
       
       #we only wait almost one program parameters list
       for(j in 1:length(parameters))
       {
         param_name <- names(parameters)[j]
-        if(param_name == "active"){
-          activeParameters <- parameters$active[[1]]
-          #control if the active parameter is boolean
-          if (!is.logical(activeParameters))
-            stop("Error: Activation parameter in orness program parameters structure must be boolean")
-        }
-        else if(param_name == "orness"){
+        if(param_name == "orness"){
           orness <- parameters$orness[[1]]
           #control if orness parameter is double and inside the [0:1] range
           if (!is.double(orness) || (orness < 0) || (orness > 1))
@@ -127,16 +120,13 @@ checkAndExtractInputs <- function(xmcdaData, programExecutionResult) {
     }
     
     if (!hasWeights && !hasParameters)
-      stop("Error: No weigths supplied ")
+      stop("Error: No weights supplied ")
     
-    if (!hasWeights & hasParameters && !activeParameters)
-      stop("Error: inactive orness supplied without alternatives ")
-    
-    if (hasWeights && hasParameters && activeParameters)
+    if (hasWeights && hasParameters)
       stop("Error: too many weights options supplied (weigths table and orness, only one is needed) ")
     
     
-    #case: weights table is provided and/or program parameters is inacive 
+    #case: weights table is provided
     if(hasWeights){
       if (xmcdaData$criteriaValuesList$size() > 1)
         stop("Error: More than one weights table supplied ")
@@ -158,7 +148,7 @@ checkAndExtractInputs <- function(xmcdaData, programExecutionResult) {
         stop("Error: different number of active weights and active criteria ")
       
     }else{
-      #case program Parameter is provided and is active
+      #case program Parameter is provided
       nCriteria <- activeCriteria$numberOfCriteria
       filteredweights <- ornessCalculation(orness, nCriteria)
       
